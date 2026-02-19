62a0329b9f225c65568c9981cda88815af1da9cf
Salman Sadi <salmansadi1010@gmail.com>
2026-02-19 14:54:51 +0600

    uploading Lab1 codes

---
+++ Test.R
@@ -0,0 +1,49 @@
+a <- 100
+a <- c(100, 200, 300)
+
+b <- c(5, 6, 7)
+sumAB <- a+b
+pdctAB <- a*b
+
+summary(b)
+mean(a)
+plot(a)
+median(c(500, 600, 700))
+var(b)
+sqrt(3546)
+sd(b)
+length(b)
+
+
+add_numbers <- function(a, b) 
+{ 
+  return(a + b) 
+}
+add_numbers(5,6)
+
+
+abs(-474)
+round(34734.34357378)
+log(1)
+exp(2)
+
+seq(a)
+rep(b)
+rep(a)
+seq(b)
+sort(c(432, 652, 192, 5632, 7, 892))
+
+u<- c("a", "b", "c", "d")
+u <- c(u, "e", "f", "g")
+append(u, "i", after = 11)
+
+
+num_vector <- c(10, 20, 30, 40, 50)
+print(num_vector[3])
+
+num_vector[3] <- 35
+num_vector[c(2,5)] <- c(25,55)
+
+num_vector[num_vector>25]
+num_vector[which(num_vector>25)]                                                                          
+
